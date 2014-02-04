02b0f816a1a003eb5f409f7a0c9f3a0a2f79f195
Dylan Wreggelsworth <dylan@bvrgroup.us>
2014-02-03 16:29:12 -0800

    working on additional assets/manifest creation

--- lib/carta/cli/compile.rb
+++ lib/carta/cli/compile.rb
@@ -37,7 +37,7 @@ module Carta
       layout_dir = "#{project_dir}/layouts"
       build_dir  = "#{project_dir}/build"
 
-      FileList.new("#{project_dir}/layouts/epub/**/*.erb").each do |layout|
+      FileList.new("#{layout_dir}/epub/**/*.erb").each do |layout|
         filename = layout.pathmap("%{^#{layout_dir},#{build_dir}}X")
         path = filename.pathmap('%d')
 
@@ -47,6 +47,34 @@ module Carta
         File.open(filename, 'w+') do |handle|
           handle.write template.result(binding)
         end
+      end
+      generate_manifest
+    end
+
+    def generate_manifest
+      layout_dir = "#{project_dir}/layouts"
+
+      files   = FileList.new("#{layout_dir}/epub/EPUB/*.erb")
+                      .exclude('**/*.opf*')
+      figures = FileList.new('manuscript/figures/**/*.{jpg,png,svg,mp4,gif}')
+      fonts  = FileList.new('assets/fonts/**/*.{otf,ttf}')
+      css  = FileList.new('assets/css/**/*.css')
+
+      files.each do |file|
+        filename = file.pathmap("%{^#{layout_dir},build}X")
+        puts "file: #{filename}"
+      end
+      figures.each do |file|
+        filename = file.pathmap("%{^manuscript/figures,build/epub/EPUB/figures}p")
+        puts "figure: #{filename}"
+      end
+      fonts.each do |file|
+        filename = file.pathmap("%{^assets/fonts,build/epub/EPUB/}p")
+        puts "font: #{filename}"
+      end
+      css.each do |file|
+        filename = file.pathmap("%{^assets/css,build/epub/EPUB/}p")
+        puts "css: #{filename}"
       end
     end
   end
